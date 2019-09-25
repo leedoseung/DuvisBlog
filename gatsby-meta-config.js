ef2347d80c9636e8923518b0653bd591d3fe42e3
leedooseung <leedoseu@gmail.com>
2019-09-25 13:18:31 +0900

    package.json 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,8 +2,8 @@ module.exports = {
   title: `Duvis Blog`,
   description: `Duvis 개발 블로그`,
   author: `DooSeungLee`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
-  siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
+  introduction: `TIL(Today I Learned) `,
+  siteUrl: `https://duvislog.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
     github: ``, // Your GitHub account
@@ -14,7 +14,7 @@ module.exports = {
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: 'DooseungLee/gatsby-starter-bee', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
